e9ff0a9301f1a794cb0c97d2b9d90f800e2618f1
Sarah <sarahhale95@gmail.com>
2018-05-09 10:13:57 +0200

    search works

--- src/App.js
+++ src/App.js
@@ -3,16 +3,62 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchedSong: '',
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+  }
+
+  handleSubmit(event) {
+    this.playSong()
+    event.preventDefault();
+  }
+
+
+  handleChange(event) {
+    this.setState({ searchedSong: event.target.value });
+  }
+  playSong() {
+    console.log("state"+this.state)
+    window.SpotifyPlayer.searchTracks(this.state.searchedSong).then(res =>
+      window.SpotifyPlayer.playTrack("spotify:track:" + res.tracks.items[0].id));
+    //  window.SpotifyPlayer.playTrack("spotify:track:3Q16RW1GJBjmdsNBd6AsH9");
+  }
+
+  pauseSong() {
+    window.SpotifyPlayer.WebPlaybackSDK.pause()
+  }
   render() {
+    console.log(this.state);
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Song:
+              <input
+                type="text"
+                value={this.state.searchedSong}
+                name="name"
+                onChange={this.handleChange}/>
+            </label>
+            <input
+
+              type="submit"
+              value="Submit"
+            />
+          </form>
+          <button onClick={this.pauseSong}>Pause</button>
+        </div>
       </div>
     );
   }
@@ -20,16 +66,6 @@ class App extends Component {
 
 if (window.SpotifyPlayer.isAccessToken() === false) {
   window.SpotifyPlayer.sendToLogin();
-} else {
-  // for (var index = 0; index < window.playlistsToLoad.length; index++) {
-  //   var playlist = window.playlistsToLoad[index];
-  //   window.SpotifyPlayer.getPlaylist(playlist.user, playlist.playlist).then((loadedPlaylist) => {
-  //     // Add songs to list
-  //     for (var i = 0; i < loadedPlaylist.tracks.items.length; i++) {
-  //       window.tracks.push(loadedPlaylist.tracks.items[i]);
-  //     }
-  //   });
-  // }
 }
 
 export default App;
