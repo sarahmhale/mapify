69207d060b317f21a15d29281076f9bd384415cd
Sofia <s_tovedal@hotmail.com>
2018-05-18 12:23:14 +0200

    added queries

--- src/api/queries/index.js
+++ src/api/queries/index.js
@@ -13,16 +13,26 @@ query markers {
 `
 
 export const CREATE_MARKER = gql`
-query markers {
-  markers {
-    id
-    longitude
-    latitude
-    icon
-    song
+mutation createMarker($marker: MarkerInput!) {
+  createMarker(marker: $marker) {
+    marker: {
+      id
+      longitude
+      latitude
+      icon
+      song
+    }
   }
 }
 `
+export const DELETE_MARKER = gql`
+mutation deleteMarker($id: Int!) {
+  createMarker(id: $id) {
+    message
+  }
+}
+`
+
 
 export const CREATE_BILLING = gql `
   mutation  createBilling($input: CreateBillingInput!) {
